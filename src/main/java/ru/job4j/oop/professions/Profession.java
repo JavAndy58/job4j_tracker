0852a997181f1ecb5ca179535f84ca3a1c4f49c2
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-19 12:47:47 +0300

    3. Реализация профессий в коде [#6837]

--- src/main/java/ru/job4j/oop/professions/Profession.java
+++ src/main/java/ru/job4j/oop/professions/Profession.java
@@ -2,8 +2,11 @@ package ru.job4j.oop.professions;
 
 public class Profession {
     private String name;
+
     private String surname;
+
     private String education;
+
     private String birthday;
 
     public String getName() {
