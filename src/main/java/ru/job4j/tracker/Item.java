701dee2693024bf26cda9cfbd67d5f42797947d1
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-19 17:13:44 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
-
     }
 
     public Item(String name) {
@@ -27,6 +29,10 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void setName(String name) {
