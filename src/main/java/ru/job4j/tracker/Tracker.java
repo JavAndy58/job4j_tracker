86099816ce5ec39fec240ecd6c8a33768920372e
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-23 10:08:56 +0300

    5. Tracker - хранилище [#396 #159180]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,16 +16,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] itemsWithoutNull = new Item[items.length];
-        int n = 0;
-        for (int i = 0; i < size; i++) {
-            Item temp = items[i];
-            if (temp.getName() != null) {
-                itemsWithoutNull[n] = temp;
-                n++;
-            }
-        }
-        return Arrays.copyOf(itemsWithoutNull, n);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
@@ -41,14 +32,28 @@ public class Tracker {
         return Arrays.copyOf(itemsKey, n);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
+        }
+        return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != - 1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            rsl = true;
         }
         return rsl;
     }
