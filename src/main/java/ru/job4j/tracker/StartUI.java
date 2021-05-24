63ffdc3dbaef00dc751cc0d60ddf5c1a14811c04
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-24 21:48:04 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,6 +16,7 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
             } else if (select == 1) {
                 System.out.println("=== Sorting an array ===");
                 Item[] temp = tracker.findAll();
