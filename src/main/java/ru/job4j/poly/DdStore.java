704744cad169160a39a0f5f007dfce288449ae3f
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-24 08:18:32 +0300

    9. toStore[#0000]

---
+++ src/main/java/ru/job4j/poly/DdStore.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly;
+
+public class DdStore implements Store{
+    public void save(String value) {
+
+    }
+
+    public String[] load() {
+        return new String[] {};
+    }
+}
