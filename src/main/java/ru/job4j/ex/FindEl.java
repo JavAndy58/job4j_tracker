d3fece4eb72f6614528e94289da517f29f34178e
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-31 17:45:37 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException{
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl == - 1) {
+            throw new ElementNotFoundException("Ключ не найден");
+        }
+        return rsl;
+    }
+        public static void main (String[]args) throws ElementNotFoundException{
+            String[] names = {"Nik", "Oleg", "Petr"};
+            try {
+                int result = indexOf(names, "Oleghh");
+                System.out.println("Индекс ключа: " + result);
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+}
