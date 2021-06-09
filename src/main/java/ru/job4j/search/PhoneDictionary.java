260cb3c40ad8d69bfbc0f1a93529a774b68587df
JavAndy58 <digital.designs.russia@gmail.com>
2021-06-09 17:42:15 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getSurname().contains(key) || person.getName().contains(key) ||
+            person.getPhone().contains(key) || person.getAddress().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PhoneDictionary phoneDictionary = new PhoneDictionary();
+        Person person1 = new Person("Petr", "Arsentev", "534872", "Bryansk");
+        phoneDictionary.add(person1);
+        ArrayList<Person> persons = phoneDictionary.find("Pet");
+        for (Person person : persons) {
+            System.out.println(person.getName() + " " + person.getSurname() + " " + person.getAddress() +
+                    " " + person.getPhone());
+        }
+    }
+}
--- src/test/java/ru/job4j/tracker/SingleTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SingleTrackerTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
@@ -12,7 +13,6 @@ public class SingleTrackerTest {
         SingleTracker singleTracker = SingleTracker.getInstance();
         SingleTracker singleTracker1 = SingleTracker.getInstance();
         assertThat(singleTracker.equals(singleTracker1), is(true));
-
     }
 
 }
