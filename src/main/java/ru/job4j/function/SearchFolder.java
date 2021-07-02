efa26e3b6392432c985cba90eff12f01a880427f
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-02 16:56:50 +0300

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -1,7 +1,7 @@
 package ru.job4j.function;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.function.Predicate;
 
---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> right.compareTo(left);
+    }
+}
