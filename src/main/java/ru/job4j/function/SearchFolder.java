797c75ec2ffc313a37c9eaf15b93813af98f66cd
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-02 11:04:41 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209 #173521]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -16,24 +16,4 @@ public class SearchFolder {
         }
         return rsl;
     }
-
-    public static void main(String[] args) {
-        List<Folder> folders = Arrays.asList(
-                new Folder("fix", 110),
-                new Folder("bug", 75),
-                new Folder("bug", 90)
-        );
-        List<Folder> foldersSize = new ArrayList<>();
-        foldersSize = SearchFolder.filter(folders, (f) -> f.getSize() > 100);
-        System.out.println("Массив после поиска");
-        for (Folder f : foldersSize) {
-            System.out.println(f);
-        }
-        List<Folder> foldersName = new ArrayList<>();
-        foldersName = SearchFolder.filter(folders, (Ff) -> Ff.getName().contains("bug"));
-        System.out.println("Массив после поиска по имени");
-        for (Folder f : foldersName) {
-            System.out.println(f);
-        }
-    }
 }
