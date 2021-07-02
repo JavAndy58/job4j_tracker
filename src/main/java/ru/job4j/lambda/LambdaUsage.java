d5513ecbb5c7ec36f435a9d08cd724aa7bf421e3
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-02 17:43:54 +0300

    2.2. Лямбда блок [#249208 #173626]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,10 +1,14 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
 
     public static void main(String[] args) {
-        Comparator<String> cmpText = (left, right) -> right.compareTo(left);
+        Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compareTo - " + right + " : " + left);
+            return right.compareTo(left);
+        };
     }
 }
