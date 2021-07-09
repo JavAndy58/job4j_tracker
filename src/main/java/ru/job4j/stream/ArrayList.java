e640cd3046668b09ff5c8a26fef814393f1df0c3
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-09 13:48:21 +0300

    0. Stream API [#58166 #175719]

--- src/main/java/ru/job4j/stream/ArrayList.java
+++ src/main/java/ru/job4j/stream/ArrayList.java
@@ -8,7 +8,7 @@ public class ArrayList {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, -2, 3, 4, 5, -6, 7, 8, -9, 10);
         List<Integer> numbers2 = numbers.stream()
-                .filter((n) -> n >= 0)
+                .filter((n) -> n > 0)
                 .collect(Collectors.toList());
     }
 }
