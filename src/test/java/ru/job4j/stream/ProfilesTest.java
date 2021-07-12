315a7ff9f432e9de92d6b90f9178a2da7c3b5531
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-12 10:49:00 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenAddressOn() {
+        List<Address> addresses = List.of(
+            new Address("Pnz", "pnn", 10, 1),
+            new Address("Mor", "ssnn", 1, 1),
+            new Address("Sar", "ssssspnn", 5, 1)
+        );
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collect(addresses.);
+    }
+}
