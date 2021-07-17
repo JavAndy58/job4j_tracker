e451a70dbdb216b9cf733a6424941b8611bbaa99
JavAndy58 <digital.designs.russia@gmail.com>
2021-07-17 11:10:42 +0300

    2. Local-Variable Type Inference [#70626 #178575]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        var index = 0;
+        for (var element : tasks) {
+            if (task.getPriority() < element.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        for (var element : queue.tasks) {
+            System.out.println(element);
+        }
+    }
+}
