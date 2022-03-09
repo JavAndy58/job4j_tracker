2bbe581ba6c1b9cdcb7c7f751873a2cd689b8fc6
JavAndy58 <digital.designs.russia@gmail.com>
2022-03-09 18:36:41 +0300

    upp

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -5,19 +5,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
 import java.util.List;
 
 public class HbmTracker implements Store, AutoCloseable {
-    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-            .configure().build();
-    private final SessionFactory sf = new MetadataSources(registry)
-            .buildMetadata().buildSessionFactory();
-
-    @Override
-    public void init(){
-
-    }
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    private final SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
     @Override
     public Item add(Item item) {
@@ -31,24 +23,12 @@ public class HbmTracker implements Store, AutoCloseable {
 
     @Override
     public boolean replace(int id, Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        
-
-
-        session.getTransaction().commit();
-        session.close();
+        return false;
     }
 
     @Override
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = new Item(null);
-        item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        return false;
     }
 
     @Override
@@ -67,7 +47,12 @@ public class HbmTracker implements Store, AutoCloseable {
     }
 
     @Override
+    public void init() {
+
+    }
+
+    @Override
     public void close() throws Exception {
-        StandardServiceRegistryBuilder.destroy(registry);
+
     }
 }
